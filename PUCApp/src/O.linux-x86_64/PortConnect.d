7aa27d0d2ec5507db29137b985744656dcc9d5b6
igor <igor.snunes@gmail.com>
2013-08-01 09:51:01 -0300

    Modifications for new db

---
+++ PUCApp/src/O.linux-x86_64/PortConnect.d
@@ -0,0 +1,35 @@
+PortConnect.o: ../PortConnect.cpp /home/igor/epics/base/include/iocsh.h \
+ /home/igor/epics/base/include/shareLib.h \
+ /home/igor/epics/base/include/epicsExport.h \
+ /home/igor/epics/base/include/shareLib.h ../PortConnect.h \
+ /home/igor/asyn/asyn4-21/include/asynPortDriver.h \
+ /home/igor/epics/base/include/epicsTypes.h \
+ /home/igor/epics/base/include/epicsMutex.h \
+ /home/igor/epics/base/include/epicsAssert.h \
+ /home/igor/epics/base/include/compilerDependencies.h \
+ /home/igor/epics/base/include/os/Linux/osdMutex.h \
+ /home/igor/asyn/asyn4-21/include/asynStandardInterfaces.h \
+ /home/igor/asyn/asyn4-21/include/asynDriver.h \
+ /home/igor/epics/base/include/epicsStdio.h \
+ /home/igor/epics/base/include/epicsTime.h \
+ /home/igor/epics/base/include/epicsTypes.h \
+ /home/igor/epics/base/include/os/Linux/osdTime.h \
+ /home/igor/epics/base/include/ellLib.h \
+ /home/igor/asyn/asyn4-21/include/asynUInt32Digital.h \
+ /home/igor/asyn/asyn4-21/include/asynInt32.h \
+ /home/igor/asyn/asyn4-21/include/asynInt8Array.h \
+ /home/igor/asyn/asyn4-21/include/asynInt16Array.h \
+ /home/igor/asyn/asyn4-21/include/asynInt32Array.h \
+ /home/igor/asyn/asyn4-21/include/asynFloat32Array.h \
+ /home/igor/asyn/asyn4-21/include/asynFloat64.h \
+ /home/igor/asyn/asyn4-21/include/asynFloat64Array.h \
+ /home/igor/asyn/asyn4-21/include/asynGenericPointer.h \
+ /home/igor/asyn/asyn4-21/include/asynEnum.h \
+ /home/igor/asyn/asyn4-21/include/asynOctet.h \
+ /home/igor/asyn/asyn4-21/include/asynDrvUser.h \
+ /home/igor/asyn/asyn4-21/include/asynOption.h \
+ /home/igor/asyn/asyn4-21/include/paramVal.h \
+ /home/igor/asyn/asyn4-21/include/asynParamType.h \
+ /home/igor/asyn/asyn4-21/include/asynDriver.h \
+ /home/igor/asyn/asyn4-21/include/asynOctetSyncIO.h ../Command.cpp \
+ ../Command.h
--- iocBoot/iocPUCIOC/st.cmd
+++ iocBoot/iocPUCIOC/st.cmd
@@ -12,10 +12,10 @@ dbLoadDatabase "dbd/PUC.dbd"
 PUC_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-dbLoadRecords("db/test.db","user=rootHost, PORT=port1, ADDRESS_VAL=20, ID_VAL=5, TYPE_VAL=0, SIZE_VAL=16386")
-drvAsynIPPortConfigure("test", "localhost:6543", 0, 0, 0);
+dbLoadRecords("db/frontendv0.db","user=rootHost, PORT=0, TIMEOUT=5")
+drvAsynIPPortConfigure("test", "localhost:6791", 0, 0, 0);
 #drvAsynSerialPortConfigure("test", "/dev/ttyACM0",0,0,0)
-portConnectConfigure("port1", "test")
+portConnectConfigure("0", "test")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
